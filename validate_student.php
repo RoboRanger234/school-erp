2075ad740fa2965f01173b02e06f21cae5f125e9
RoboRanger234 <88830908+RoboRanger234@users.noreply.github.com>
2023-04-28 20:31:58 +0530

    Update validate_student.php
    Just updating the script with the desired code

--- validate_student.php
+++ validate_student.php
@@ -3,22 +3,22 @@
   $connection_failed = false;
   $conn = mysqli_connect("localhost","root","","school_info_database") or $connection_failed = true;
   if($conn == $connection_failed){
-      echo"Connection failed";
+      echo"DB down";
       print(mysqli_error($conn));
   }
   $result_failed = false;
-  $result= mysqli_query($conn,"Select school_assigned_student_id,first_name,last_name,telephone_number,email,class,dob FROM school_info_database.student_class_info where school_assigned_student_id = '$student_id'") or $result_failed=true;
+  $result= mysqli_query($conn,"Select school_assigned_student_id,first_name,last_name,telephone_number,email,class,dob FROM school_info_database.student_class_info where school_assigned_student_id = '$student_id';") or $result_failed=true;
   if($result_failed == true){
       echo("Not working");
       print(mysqli_error($conn));
-      if(mysqli_num_rows($result)>0){
+      if(mysqli_num_rows($result) == 1){
           while($row = mysqli_fetch_assoc($result)) {
               print_r($row);
           }
           echo("You are already registered you can update the form as well through here: <a href='update.html'>Update yourself</a>");
       }
-  }
-  else{
-      echo("You are not registered you can get yourself registered from here: <a href='sign_up.html'>New Admission</a>");
+      else{
+          echo("You are not registered you can get yourself registered from here: <a href='sign_up.html'>New Admission</a>");
+      }
   }
 ?>
